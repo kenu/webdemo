6c966dbbc1aad996514e6f57c09d0a07cf89ae57
kenu <kenu.heo@gmail.com>
2021-01-29 16:00:28 +0900

    debugging sample

--- src/main/java/com/example/webdemo/WebdemoApplication.java
+++ src/main/java/com/example/webdemo/WebdemoApplication.java
@@ -3,11 +3,20 @@ package com.example.webdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class WebdemoApplication {
 
 	public static void main(String[] args) {
+		long logTime = getCurrentTime();
+		System.out.println("2021/01/29 " + logTime);
 		SpringApplication.run(WebdemoApplication.class, args);
+		System.out.println("duration: " + (getCurrentTime() - logTime));
+	}
+
+	private static long getCurrentTime() {
+		return new Date().getTime();
 	}
 
 }
